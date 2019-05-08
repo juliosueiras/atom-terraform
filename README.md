5f31579e3189ecae4fab828baaf5f162d8609b60
Julio Tain Sueiras <juliosueiras@gmail.com>
2019-05-08 16:12:38 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Atom Terraform LSP Client
+
+Mostly adapted from [atom-solargraph](https://github.com/castwide/atom-solargraph)
+
---
+++ spec/atom-terraform-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomTerraformView from '../lib/atom-terraform-view';
+
+describe('AtomTerraformView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
